5986f279e9f099e71f6bbfdb2bfa0945531c426f
Anemily Machina <anemily.machina@gmail.com>
2026-08-24 16:05:30 -0400

    checking kmeans loss

--- src/experiment_subsampling_multi.py
+++ src/experiment_subsampling_multi.py
@@ -479,7 +479,7 @@ def test_kmeans():
 
     # for sub_rate, num_trials in zip(subsample_rates, number_of_trials):
 
-    max_iter = 300
+    max_iter = 3  # 00
 
     kmeans_params = {
         "n_clusters": 20000,
