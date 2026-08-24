2ac7ff4dd30c729cc3d8007848accf8e18ae8d23
Anemily Machina <anemily.machina@gmail.com>
2026-08-24 12:32:54 -0400

    checking kmeans loss

--- src/experiment_subsampling_multi.py
+++ src/experiment_subsampling_multi.py
@@ -491,7 +491,7 @@ def test_kmeans():
 
     for sub_rate in subsample_rates:
 
-        for ratio in [0.99]:
+        for ratio in [0.1, 0.01, 0.001, 0.0001, 0.00001, 0.0]:
 
             kmeans_params["reassignment_ratio"] = ratio
 
