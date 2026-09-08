6e5e95c5d393bd195cdbec71fa872ffbb95a8b4b
Anemily Machina <anemily.machina@gmail.com>
2026-09-08 13:17:52 -0400

    scoring kmeans stability

--- src/experiment_subsampling_multi.py
+++ src/experiment_subsampling_multi.py
@@ -462,8 +462,8 @@ def mean_std_experiments():
 def compare_clusters(cluster1, cluster2):
 
     # tested and torch was faster than numpy (30 vs 28)
-    cluster1 = torch.tensor(cluster1, dtype=torch.float16)
-    cluster2 = torch.tensor(cluster2, dtype=torch.float16)
+    cluster1 = torch.tensor(cluster1, dtype=torch.float32)
+    cluster2 = torch.tensor(cluster2, dtype=torch.float32)
 
     # print(cluster1)
     # print(cluster2)
