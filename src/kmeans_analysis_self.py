c545f5f348796e809bd4a2dffef4baf6c2659638
Anemily Machina <anemily.machina@gmail.com>
2026-09-14 13:31:31 -0400

    fixed l2 computations, converted to from [tensor].fn to torch.fn([tensor]) in hopes that will fix the memory issues

--- src/kmeans_analysis_self.py
+++ src/kmeans_analysis_self.py
@@ -72,11 +72,17 @@ def compare_clusters(cluster1, cluster2):
         l1_i = torch.mean(abs, dim=1)
         l1.append(l1_i)
 
+        print(abs[0])
         sqr = torch.pow(abs, 2)
+        print(sqr[0])
+
         sqr_sum = torch.sum(sqr, dim=1)
+        print(sqr_sum[0])
         l2_i = torch.sqrt(sqr_sum)
+        print(l2_i)
         # L2 amoratized across dimensions
         l2_i = l2_i / len(vec_i)
+        print(l2_i, len(vec_i))
         l2.append(l2_i)
 
         vec_i_norm = torch.nn.functional.normalize(vec_i, 2, dim=0)
