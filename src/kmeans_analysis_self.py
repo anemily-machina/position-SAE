76cabb1099c09376dea073d5dc89eab7a0f30862
Anemily Machina <anemily.machina@gmail.com>
2026-09-15 14:49:11 -0400

    comparisons vs baseline

--- src/kmeans_analysis_self.py
+++ src/kmeans_analysis_self.py
@@ -65,9 +65,7 @@ def compare_clusters(cluster1, cluster2):
     norm_cluster_1 = torch.nn.functional.normalize(cluster1, 2, dim=1)
     norm_cluster_2 = torch.nn.functional.normalize(cluster2, 2, dim=1)
 
-    print()
-    print("calc L1")
-    print()
+    cosine = norm_cluster_2 @ norm_cluster_1
 
     for v_i, vec_i in tqdm(enumerate(cluster1), total=len(cluster1), ncols=50):
 
@@ -79,16 +77,6 @@ def compare_clusters(cluster1, cluster2):
         l1_i = l1_i.clone()
         l1.append(l1_i)
 
-    print()
-    print("calc L2")
-    print()
-
-    for v_i, vec_i in tqdm(enumerate(cluster1), total=len(cluster1), ncols=50):
-
-        diff = cluster2 - vec_i
-
-        abs = torch.abs(diff)
-
         sqr = abs * abs
         sqr_sum = torch.sum(sqr, dim=1)
         l2_i = torch.sqrt(sqr_sum)
@@ -97,17 +85,16 @@ def compare_clusters(cluster1, cluster2):
         l2_i = l2_i.clone()
         l2.append(l2_i)
 
-    print()
-    print("calc cosine")
-    print()
-
-    for v_i, vec_i in tqdm(enumerate(cluster1), total=len(cluster1), ncols=50):
-
         vec_i_norm = norm_cluster_1[v_i]
         vec_i_norm_t = torch.t(vec_i_norm)
         cosine_i = norm_cluster_2 @ vec_i_norm_t
         cosine_i = cosine_i.clone()
         cosine.append(cosine_i)
+
+        print(cosine_i)
+        print(cosine[v_i])
+
+        exit()
 
     score_info = {
         "L1": {
