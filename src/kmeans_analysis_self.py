0cb4b65092f5ae38d1fb1d221ac4e305ded7a329
Anemily Machina <anemily.machina@gmail.com>
2026-09-18 12:34:22 -0400

    sperating scores so we lose less when having to restart

--- src/kmeans_analysis_self.py
+++ src/kmeans_analysis_self.py
@@ -88,23 +88,15 @@ def _compute_L1(cluster1: torch.Tensor, cluster2: torch.Tensor):
     )
     sum_vec = torch.zeros((len(cluster1)), dtype=cluster1.dtype, device=cluster1.device)
 
-    c = 1
     for vec_i in tqdm(cluster1, total=len(cluster1), ncols=50):
-        c += 1
-        if c == 10:
-            exit()
 
-        diff = cluster2 - vec_i
         torch.subtract(cluster2, vec_i, out=diff_matrix)
 
-        print(diff[0][:30])
-        print(diff_matrix[0][:30])
-
-        torch.abs_(diff)
+        torch.abs_(diff_matrix)
 
         # L1 amoratized across dimensions
         # why is this so much faster than using torch.mean 150it/s vs 450it/s?
-        torch.sum(diff, dim=1, out=sum_vec)
+        torch.sum(diff_matrix, dim=1, out=sum_vec)
         l1_i = sum_vec / vec_size
 
         l1.append(l1_i)
