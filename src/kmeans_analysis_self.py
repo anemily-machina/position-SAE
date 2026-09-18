223a1772f7ce33a94a9719e58904e45458a22110
Anemily Machina <anemily.machina@gmail.com>
2026-09-18 12:15:02 -0400

    sperating scores so we lose less when having to restart

--- src/kmeans_analysis_self.py
+++ src/kmeans_analysis_self.py
@@ -84,14 +84,15 @@ def _compute_L1(cluster1, cluster2):
 
         diff = cluster2 - vec_i
 
-        # torch.abs_(diff)
+        torch.abs_(diff)
 
         # L1 amoratized across dimensions
+        # why is this so much faster than using torch.mean?
         l1_i = torch.sum(diff, dim=1)
 
         l1_i = l1_i / len(vec_i)
 
-        # l1.append(l1_i)
+        l1.append(l1_i)
 
     # cost_matrix = torch.stack(l1)
 
