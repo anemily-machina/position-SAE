dc69f2364b6fac09778c55c9fafdaa2ff1d04370
Anemily Machina <anemily.machina@gmail.com>
2026-09-18 12:21:31 -0400

    sperating scores so we lose less when having to restart

--- src/kmeans_analysis_self.py
+++ src/kmeans_analysis_self.py
@@ -92,7 +92,7 @@ def _compute_L1(cluster1: torch.Tensor, cluster2: torch.Tensor):
         # L1 amoratized across dimensions
         # why is this so much faster than using torch.mean 150it/s vs 450it/s?
         l1_i = torch.sum(diff, dim=1)
-        l1_i = l1_i / len(vec_i)
+        # l1_i = l1_i / len(vec_i)
 
         torch.sum(diff, dim=1, out=sum_vec)
 
