b27bd890c94b6890f730fdea6e17c6998ef3a5c9
Anemily Machina <anemily.machina@gmail.com>
2026-09-20 17:45:07 -0400

    quick fix to change original scores to seperated files

--- src/kmeans_analysis_self.py
+++ src/kmeans_analysis_self.py
@@ -413,7 +413,7 @@ if __name__ == "__main__":
                     score_file_name = f"{stats_file_name_prefix}_{score_key}.pkl"
                     score_fname = os.path.join(stats_folder, score_file_name)
 
-                    score2 = load_pickle(score, score_fname)
+                    score2 = load_pickle(score_fname)
 
                     print(score_key, score, score2)
 
